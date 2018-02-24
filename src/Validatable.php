ab08afc4fb9325a9c2d68c85bd839a48545c9040
fsubal <fsubal1102@gmail.com>
2018-02-24 17:47:31 +0900

    Exception was namespaced ><

--- src/Validatable.php
+++ src/Validatable.php
@@ -48,12 +48,12 @@ trait Validatable
     private static function _execTypeCheck(array $params)
     {
         if (!defined('static::REQUIRED')) {
-            throw new LogicException(static::class . ' has no private const REQUIRED.');
+            throw new \LogicException(static::class . ' has no private const REQUIRED.');
         }
 
         foreach (static::REQUIRED as $prop => $type) {
             if (!isset($params[$prop])) {
-                throw new InvalidArgumentException(static::class . ": params['{$prop}'] is not set.");
+                throw new \InvalidArgumentException(static::class . ": params['{$prop}'] is not set.");
             }
             static::_assertTypeOfPropValue($prop, $params[$prop], $type);
         }
@@ -75,12 +75,12 @@ trait Validatable
         }
 
         if (in_array($type, static::$_DEFAULT_FORBIDDEN_TYPES, true)) {
-            throw new LogicException("using '{$type}' is not allowed (params['{$prop}'])");
+            throw new \LogicException("using '{$type}' is not allowed (params['{$prop}'])");
         }
 
         foreach (static::$_DEFAULT_FORBIDDEN_TYPES as $forbidden_type) {
             if (is_subclass_of($type, $forbidden_type)) {
-                throw new LogicException("using '{$type}' (subclass of {$forbidden_type}) is not allowed (params['{$prop}'])");
+                throw new \LogicException("using '{$type}' (subclass of {$forbidden_type}) is not allowed (params['{$prop}'])");
             }
         }
 
@@ -91,11 +91,11 @@ trait Validatable
                 return $value instanceof $type;
             };
         } else {
-            throw new LogicException("params['{$prop}'] has unknown type: {$type}");
+            throw new \LogicException("params['{$prop}'] has unknown type: {$type}");
         }
 
         if (call_user_func($validator, $value) !== true) {
-            throw new TypeError("params['{$prop}'] is not type of {$type}");
+            throw new \TypeError("params['{$prop}'] is not type of {$type}");
         }
     }
 }
