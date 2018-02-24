06e4cd493a856765f5808c6f9feea88436fdb7d6
fsubal <fsubal1102@gmail.com>
2018-02-24 17:47:04 +0900

    add phpfmt

--- tests/Immutable/ImmutableFromTest.php
+++ tests/Immutable/ImmutableFromTest.php
@@ -4,17 +4,17 @@ namespace Donphan;
 
 final class ImmutableFromTest extends \PHPUnit\Framework\TestCase
 {
-    public function test_from_expectedly_works_with_required ()
+    public function test_from_expectedly_works_with_required()
     {
         $date = new \DateTimeImmutable();
         $immutable = TestEntity::from([
-            'int'     => 1,
-            'float'   => 1.0,
+            'int' => 1,
+            'float' => 1.0,
             'numeric' => '1000',
-            'string'  => 'hello!',
+            'string' => 'hello!',
             'boolean' => true,
-            'array'   => [],
-            'date'    => $date,
+            'array' => [],
+            'date' => $date,
         ]);
 
         $this->assertSame($immutable->int, 1);
@@ -26,24 +26,24 @@ final class ImmutableFromTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($immutable->date, $date);
     }
 
-    public function test_from_expectedly_works_with_optional ()
+    public function test_from_expectedly_works_with_optional()
     {
         $date = new \DateTimeImmutable();
         $immutable = TestEntity::from([
-            'int'      => 1,
-            'float'    => 1.0,
-            'numeric'  => '1000',
-            'string'   => 'hello!',
-            'boolean'  => true,
-            'array'    => [],
-            'date'     => $date,
-            '_int'     => 1,
-            '_float'   => 1.0,
+            'int' => 1,
+            'float' => 1.0,
+            'numeric' => '1000',
+            'string' => 'hello!',
+            'boolean' => true,
+            'array' => [],
+            'date' => $date,
+            '_int' => 1,
+            '_float' => 1.0,
             '_numeric' => '1000',
-            '_string'  => 'hello!',
+            '_string' => 'hello!',
             '_boolean' => true,
-            '_array'   => [],
-            '_date'    => $date,
+            '_array' => [],
+            '_date' => $date,
         ]);
 
         $this->assertSame($immutable->_int, 1);
@@ -55,36 +55,36 @@ final class ImmutableFromTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($immutable->_date, $date);
     }
 
-    public function test_beforeTypeCheck_expectedly_works ()
+    public function test_beforeTypeCheck_expectedly_works()
     {
         $date = new \DateTimeImmutable();
         $immutable = TestEntity::from([
-            'int'      => 1,
-            'float'    => 1.0,
-            'numeric'  => '1000',
-            'string'   => 'hello!',
-            'boolean'  => true,
-            'date'     => $date,
+            'int' => 1,
+            'float' => 1.0,
+            'numeric' => '1000',
+            'string' => 'hello!',
+            'boolean' => true,
+            'date' => $date,
         ]);
 
-        $this->assertSame($immutable->array, TestEntity::DEFAULT['array']);
+        $this->assertSame($immutable->array, TestEntity::DEFAULTS['array']);
     }
 
     /**
      * @expectedException \InvalidArgumentException
      * @expectedExceptionMessage params[string] must not be empty
      */
-    public function test_afterTypeCheck_expectedly_works ()
+    public function test_afterTypeCheck_expectedly_works()
     {
         $date = new \DateTimeImmutable();
         $immutable = TestEntity::from([
-            'int'      => 1,
-            'float'    => 1.0,
-            'numeric'  => '1000',
-            'string'   => '',
-            'boolean'  => true,
-            'array'    => [],
-            'date'     => $date,
+            'int' => 1,
+            'float' => 1.0,
+            'numeric' => '1000',
+            'string' => '',
+            'boolean' => true,
+            'array' => [],
+            'date' => $date,
         ]);
     }
 }
--- tests/TestEntity.php
+++ tests/TestEntity.php
@@ -2,39 +2,40 @@
 
 namespace Donphan;
 
-final class TestEntity {
+final class TestEntity
+{
     use Immutable;
 
     const REQUIRED = [
-        'int'     => 'int',
-        'float'   => 'float',
+        'int' => 'int',
+        'float' => 'float',
         'numeric' => 'numeric',
-        'string'  => 'string',
+        'string' => 'string',
         'boolean' => 'boolean',
-        'array'   => 'array',
-        'date'    => '\DateTimeImmutable',
+        'array' => 'array',
+        'date' => '\DateTimeImmutable',
     ];
 
     const OPTIONAL = [
-        '_int'     => 'int',
-        '_float'   => 'float',
+        '_int' => 'int',
+        '_float' => 'float',
         '_numeric' => 'numeric',
-        '_string'  => 'string',
+        '_string' => 'string',
         '_boolean' => 'boolean',
-        '_array'   => 'array',
-        '_date'    => '\DateTimeImmutable',
+        '_array' => 'array',
+        '_date' => '\DateTimeImmutable',
     ];
 
-    const DEFAULT = [
-        'array' => [1, 2, 3, 4, 5]
+    const DEFAULTS = [
+        'array' => [1, 2, 3, 4, 5],
     ];
 
-    private static function beforeTypeCheck (array $params)
+    private static function beforeTypeCheck(array $params)
     {
-        return array_merge(self::DEFAULT, $params);
+        return array_merge(self::DEFAULTS, $params);
     }
 
-    private static function afterTypeCheck (array $params)
+    private static function afterTypeCheck(array $params)
     {
         if (strlen($params['string']) == 0) {
             throw new \InvalidArgumentException('params[string] must not be empty');
